263daa4a562b81a2c8687a2eba09dc5dd6d56d44
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-03-12 10:26:06 +0530

    Create getTargetFrAch.php
    addd this file

---
+++ getTargetFrAch.php
@@ -0,0 +1,25 @@
+{
+  "result": "true",
+  "target": [
+    {
+      "daily_target": "236410",
+      "productivity_target": "32",
+      "callage_target": "0",
+      "day_achievement": "137828.85",
+      "must_sku": [
+        {
+          "item_id": 170,
+          "item_distribution_target": "10"
+        },
+        {
+          "item_id": 384,
+          "item_distribution_target": "12"
+        },
+        {
+          "item_id": 406,
+          "item_distribution_target": "15"
+        }
+      ]
+    }
+  ]
+}
