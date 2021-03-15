7dbce4163fa425f617f06f8fa64c9c8eee39dcc9
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-03-15 16:18:25 +0530

    Create getUniqueOutletCountDayReport.php
    add this service

---
+++ getUniqueOutletCountDayReport.php
@@ -0,0 +1,25 @@
+{
+    "result": true,
+    "data": [
+        {
+            "territory": "NITTAMBUWA",
+            "timestamp": 1615804018,
+            "unique_outlet_count": [  
+                {
+                    "item_id": 1,  
+                    "item_name": "",
+                    "sr_out_count": "8",
+                    "today_invo_count": 4,
+                    "unique_out_count": 4
+                },
+                {
+                    "item_id": 2,
+                    "item_name": "",
+                    "sr_out_count": "24",
+                    "today_invo_count": 22,
+                    "unique_out_count": 21
+                }
+            ]
+        }
+    ]
+}
