e59fd9d4d634568c2f6f154d38980b98651bbd2d
rizlan_dev <38829499+rizlandev@users.noreply.github.com>
2021-03-16 16:19:42 +0530

    Create mtdReportView.php
    asasad

---
+++ mtdReportView.php
@@ -0,0 +1,68 @@
+{
+  "result": true,
+  "mtd_target": [
+    {
+      "territory": "NITTAMBUWA",
+      "timestamp": 1615433259,
+      "month_tar": 4530671.7,
+      "month_day_tar": "1996471",
+      "month_day_arch": 1945301.57,
+      "num_of_day": "9/21",
+      "collage_tar": 1225,
+      "collage_arch_old": 510,
+      "collage_arch": 566,
+      "productivity_tar": 860,
+      "productivity_arch_old": 336,
+      "productivity_arch": 338,
+      "volume_tar_ache": [],  // remove this volume calculations 
+      "distribution_tar_ache": [ 
+        {
+          "item_id": 1,
+          "item_name": "",
+          "d_month_tar": "300",
+          "d_month_day_tar": "135",
+          "d_month_day_arch": 97
+        },
+        {
+          "item_id": 3,
+          "item_name": "",
+          "d_month_tar": "620",
+          "d_month_day_tar": "277",
+          "d_month_day_arch": 238
+        },
+        {
+          "item_id": 6,
+          "item_name": "",
+          "d_month_tar": "420",
+          "d_month_day_tar": "172",
+          "d_month_day_arch": 171
+        }
+      ],
+      "UniqueOutletDistribution_tar_ache": [
+        {
+          "item_id": 1,
+          "item_name": "",
+          "u_month_tar": 0,
+          "u_month_day_tar": "135",
+          "u_month_day_arch": 96
+        },
+        {
+          "item_id": 3,
+          "item_name": "",
+          "u_month_tar": 0,
+          "u_month_day_tar": "277",
+          "u_month_day_arch": 234
+        },
+        {
+          "item_id": 6,
+          "item_name": "",
+          "u_month_tar": 0,
+          "u_month_day_tar": "172",
+          "u_month_day_arch": 169
+        }
+      ],
+      "sku_bill_tar": 0,
+      "sku_bill_arch": "2.2515"
+    }
+  ]
+}
